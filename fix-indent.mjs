0fe9075f9c143f4fc566084a6955f091d347abf1
Joshua123895 <174108616+Joshua123895@users.noreply.github.com>
2026-07-09 00:27:32 +0900

    deleted useless files

--- fix-indent.mjs
+++
@@ -1,7 +0,0 @@
-import { load, dump } from 'js-yaml';
-import fs from 'fs';
-const yaml = fs.readFileSync('./src/data/tracks.yaml', 'utf-8');
-const data = load(yaml);
-const fixed = dump(data, { indent: 2, lineWidth: 120 });
-fs.writeFileSync('./src/data/tracks.yaml', fixed, 'utf-8');
-console.log('Fixed and re-written');
